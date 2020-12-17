789f797827ce86a008dfdeb57fb79abb819da0ba
popop098 <53284350+popop098@users.noreply.github.com>
2020-12-17 13:05:22 +0900

    Add files via upload

---
+++ Example/upguild.py
@@ -0,0 +1,13 @@
+import PostGuilds
+import discord
+from discord.ext import commands
+
+class GuildCount(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = "UniqueBot TOKEN"
+        PostGuilds.UpdateGuilds(self.bot,self.token)
+
+def setup(bot):
+    bot.add_cog(GuildCount(bot))
